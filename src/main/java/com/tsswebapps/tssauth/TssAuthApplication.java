71af4378ed13fc0ff934f9a731d7976234aa6891
xthiagosant <uzmaki_thiago@outllok.com>
2022-03-06 20:28:02 -0300

    Iniciando o projeto da semana

---
+++ src/main/java/com/tsswebapps/tssauth/TssAuthApplication.java
@@ -0,0 +1,13 @@
+package com.tsswebapps.tssauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TssAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TssAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsswebapps/tssauth/TssAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsswebapps.tssauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TssAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
